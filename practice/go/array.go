4bec2d5db5c432e8917d896bc6cf5cde87e6d365
root <root@localhost.localdomain>
2021-03-02 06:13:52 -0500

    golang array op

---
+++ practice/go/array.go
@@ -0,0 +1,14 @@
+package main
+
+import ("fmt"
+            )
+
+func main(){
+
+    a := 3 
+    var b[2]int;
+    b[0] = 199;
+
+    var c = [6]int{3, 4, 5, 2, 4}
+    fmt.Println("hello", a, b, c, c+2)
+}
